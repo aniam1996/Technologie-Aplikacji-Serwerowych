f304404712ba26b2c2f8117d074ae3002ec3b1a9
Dabi <agatha1760@gmail.com>
2017-11-28 14:48:42 +0100

    TASverTEST2

---
+++ java/com/example/agatha/tasvertest2/MainActivity.java
@@ -0,0 +1,182 @@
+package com.example.agatha.tasvertest2;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    public String u_email = "Login to fully explore the site!";
+    public String u_username = "Guest";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+/*
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+*/
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("USER_PREFERENCES", MODE_PRIVATE);
+        if(pref.getString("user_email", null)!=null) {
+            u_email = pref.getString("user_email", null);
+            SharedPreferences.Editor editor = pref.edit();
+            editor.putBoolean("logged", true);
+            editor.commit();
+        }
+        if(pref.getString("user_username", null)!= null) {
+            u_username = pref.getString("user_username", null);
+        }
+        View headerView = navigationView.getHeaderView(0);
+        TextView emailView = (TextView)headerView.findViewById(R.id.emailView);
+        TextView usernameView = (TextView)headerView.findViewById(R.id.usernameView);
+        emailView.setText(u_email);
+        usernameView.setText(u_username);
+        Context context = getApplicationContext();
+        Toast toast = Toast.makeText(context, u_email, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("USER_PREFERENCES", MODE_PRIVATE);
+        if(pref.getString("user_email", null)!=null) {
+            u_email = pref.getString("user_email", null);
+            SharedPreferences.Editor editor = pref.edit();
+            editor.putBoolean("logged", true);
+            editor.commit();
+        }
+        if(pref.getString("user_username", null)!= null) {
+            u_username = pref.getString("user_username", null);
+        }
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        View headerView = navigationView.getHeaderView(0);
+        TextView emailView = (TextView)headerView.findViewById(R.id.emailView);
+        TextView usernameView = (TextView)headerView.findViewById(R.id.usernameView);
+        emailView.setText(u_email);
+        usernameView.setText(u_username);
+        Context context = getApplicationContext();
+        Toast toast = Toast.makeText(context, u_email, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        } else if (id == R.id.action_logout){
+            u_username = "Guest";
+            u_email = "Login to fully explore the site!";
+            SharedPreferences pref = getApplicationContext().getSharedPreferences("USER_PREFERENCES", MODE_PRIVATE);
+            SharedPreferences.Editor editor = pref.edit();
+            editor.putString("user_email", null);
+            editor.putString("user_username", null);
+            editor.putBoolean("logged", false);
+            editor.commit();
+            NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+            View headerView = navigationView.getHeaderView(0);
+            TextView emailView = (TextView)headerView.findViewById(R.id.emailView);
+            TextView usernameView = (TextView)headerView.findViewById(R.id.usernameView);
+            emailView.setText(u_email);
+            usernameView.setText(u_username);
+            Context context = getApplicationContext();
+            Toast toast = Toast.makeText(context, u_email, Toast.LENGTH_LONG);
+            toast.show();
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_profile) {
+            // Handle the camera action
+        } else if (id == R.id.nav_explore) {
+
+        } else if (id == R.id.nav_cart) {
+
+        } else if (id == R.id.nav_history) {
+
+        } else if (id == R.id.nav_login){
+            SharedPreferences pref = getApplicationContext().getSharedPreferences("USER_PREFERENCES", MODE_PRIVATE);
+            SharedPreferences.Editor editor = pref.edit();
+            if(!pref.getBoolean("logged", true)) {
+                Intent intent = new Intent(this, LoginActivity.class);
+                startActivity(intent);
+            } else {
+                Context context = getApplicationContext();
+                Toast toast = Toast.makeText(context, "You're already logged in!", Toast.LENGTH_LONG);
+                toast.show();
+            }
+
+
+        } else if (id == R.id.nav_manage) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
